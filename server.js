31d94382a633bd182ce15307169079411f94ab0f
mhamad <mhamadasmarrr@gmail.com>
2022-10-31 18:58:26 +0200

    file post method to move images to uploaded folder (backend)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const userRoutes = require('./routes');
+const multer = require('multer');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +14,36 @@ app.use(cors());
 app.get("/", function(re,res){
     res.send("express here!")
 });
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../Employee Directory/frontend/public', 'uploads'),
+    filename: function (req, file, cb) {   
+        cb(null, file.originalname )  
+    }
+})
+
+
+app.post('/imageupload', async (req, res) => {	
+    try {
+
+        let upload = multer({ storage: storage}).single('avatar');
+
+        upload(req, res, function(err) {
+
+            if (!req.file) {
+                return res.send('Please select an image to upload');
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+            
+        }); 
+
+    }catch (err) {console.log(err)}
+})
 
 app.use('/api/v1/users', userRoutes);
 
